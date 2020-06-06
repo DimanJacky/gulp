0528fa44d3bcebd004445a5d7c4b41acb9cee012
DimanJacky <maslov-mda@yandex.ru>
2020-06-07 01:06:24 +0300

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,8 @@
 var gulp = require('gulp');
-/*
-gulp.task('mytask', async function() {
-	console.log('Привет, я таск!');
+sass = require('gulp-sass'); //Подключаем Sass пакет
+
+gulp.task('sass', function(){ // Создаем таск "sass"
+	return gulp.src('app/sass/main.sass') // Берем источник
+		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 });
-*/
-
-gulp.task('mytask', function () {
-  return gulp.src('source-files') // Выборка исходных файлов для обработки плагином
-    .pipe(plugin()) // Вызов Gulp плагина для обработки файла
-    .pipe(gulp.dest('folder')) // Вывод результирующего файла в папку назначения (dest - пункт назначения)
-})
-
