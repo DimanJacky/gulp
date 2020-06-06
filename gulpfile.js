9bfacc79206c6078500c39495b6a9fdb03f4757b
DimanJacky <maslov-mda@yandex.ru>
2020-06-07 00:34:56 +0300

    add tack

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+/*
+gulp.task('mytask', async function() {
+	console.log('Привет, я таск!');
+});
+*/
+
+gulp.task('mytask', function () {
+  return gulp.src('source-files') // Выборка исходных файлов для обработки плагином
+    .pipe(plugin()) // Вызов Gulp плагина для обработки файла
+    .pipe(gulp.dest('folder')) // Вывод результирующего файла в папку назначения (dest - пункт назначения)
+})
+
