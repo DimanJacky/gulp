f821c9382abea95f131925ab5951ccb86f844cc2
DimanJacky <maslov-mda@yandex.ru>
2020-06-07 01:21:50 +0300

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 sass = require('gulp-sass'); //Подключаем Sass пакет
 
 gulp.task('sass', function(){ // Создаем таск "sass"
-	return gulp.src('app/sass/main.sass') // Берем источник
+	return gulp.src('app/sass/**/*.sass') // Берем все sass файлы из папки sass и дочерних, если таковые будут
 		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
 		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 });
